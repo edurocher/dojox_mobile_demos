2915bce2dbaefa33edd6ab4f60cfd7a5463df9bb
unknown <A_Vasiliu@.(none)>
2012-03-28 19:31:31 +0200

    Added Accordion demo (initial version)

---
+++ demos/mobileGallery/src/accordion.js
@@ -0,0 +1,15 @@
+define(["dojox/mobile/parser",
+		"dojox/mobile",
+		"dojox/mobile/deviceTheme",
+		"dojox/mobile/compat",
+		"dojox/mobile/Accordion",
+		"dojox/mobile/ContentPane",
+		"dojox/mobile/TabBar",
+		"dojox/mobile/ScrollableView"], 
+		function(dom, on, registry) {
+	return {
+		init: function(){
+			// nothing
+		}
+	};
+});
--- demos/mobileGallery/src/structure.js
+++ demos/mobileGallery/src/structure.js
@@ -106,6 +106,14 @@ define(["dojo/_base/lang"],
 				title: "Media",
 				demourl: "views/mobileMedia.html"
 			}, {
+				id: "accordion",
+				speclevel: "8",
+				iconPos: "203,0,29,29",
+				title: "Accordion",
+				demourl: "views/accordion.html",
+				jsmodule: "demos/mobileGallery/src/accordion",
+				jsSrc: "doc/src/accordion.js.txt"
+			}, {			
 				id: "map",
 				speclevel: "6",
 				iconPos: "174,0,29,29",
