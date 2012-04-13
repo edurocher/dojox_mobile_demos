a5fbb5a15fd2cdd8599c9435769de31bb994af1a
Soyhy Lim <soyhyl@gmail.com>
2012-04-13 11:53:47 +0200

    minor changes on Dialogs demo

--- demos/mobileGallery/src/dialogs.js
+++ demos/mobileGallery/src/dialogs.js
@@ -1,7 +1,8 @@
 require(["dojo/dom", 
 	"dijit/registry",
 	"dojox/mobile/ProgressIndicator",
-	"dojox/mobile/parser", "dojox/mobile",
+	"dojox/mobile/parser", 
+	"dojox/mobile",
 	"dojox/mobile/compat",
 	"dojox/mobile/SimpleDialog",
 	"dojox/mobile/TextBox",
@@ -19,18 +20,18 @@ require(["dojo/dom",
 
 	var prog;
 
-	show_progress_indicator = function(dlg, cont) {
+	show_progress_indicator = function(dlg, cont){
 		show(dlg);
 		var container = dom.byId(cont);
 		prog = ProgressIndicator.getInstance();
 		container.appendChild(prog.domNode);
 		prog.start();
-		setTimeout(function() {
+		setTimeout(function(){
 			hide_progress_indicator(dlg);
 		}, 5000);
 	}
 
-	hide_progress_indicator = function(dlg) {
+	hide_progress_indicator = function(dlg){
 		prog.stop();
 		hide(dlg);
 	}
